44caf98934d2bc45b22d8c0dee49fd4dbce757c1
Billy Morrison <billy.morrison1234@hotmail.co.uk>
2020-10-04 17:58:40 +0100

    completed boolean exercises

--- src/app.js
+++ src/app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const { sayHello, uppercase, lowercase, firstCharacters } = require('./lib/strings');
 const { add, subtract, divide, multiply, remainder } = require('./lib/numbers');
+const { negate, truthiness, isOdd, startsWith } = require('./lib/booleans');
 
 const app = express();
 
@@ -12,6 +13,14 @@ function checkNumbersFromParameters(req, res) {
 
   if (Number.isNaN(num1) || Number.isNaN(num2)) {
     res.status(400).json({ error: 'Parameters must be valid numbers.' });
+  }
+}
+
+function checkOneNumberFromParameters(req, res) {
+  const num = Number(req.params.num);
+
+  if (Number.isNaN(num)) {
+    res.status(400).json({ error: 'Parameter must be a number.' });
   }
 }
 
@@ -94,6 +103,27 @@ app.post('/numbers/remainder', (req, res) => {
   checkForZero(res, num2);
 
   res.status(200).json({ result: remainder(num1, num2) });
+});
+
+// Booleans
+
+app.post('/booleans/negate', (req, res) => {
+  res.status(200).json({ result: negate(req.body.value) });
+});
+app.post('/booleans/truthiness', (req, res) => {
+  res.status(200).json({ result: truthiness(req.body.value) });
+});
+app.get('/booleans/is-odd/:num', (req, res) => {
+  checkOneNumberFromParameters(req, res);
+  res.status(200).json({ result: isOdd(req.params.num) });
+});
+app.get('/booleans/:word/starts-with/:char', (req, res) => {
+  const { char, word } = req.params;
+
+  if (char.length > 1) {
+    res.status(400).json({ error: 'Parameter "character" must be a single character.' });
+  }
+  res.status(200).json({ result: startsWith(char, word) });
 });
 
 module.exports = app;
